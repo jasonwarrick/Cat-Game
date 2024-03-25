ccddb4a104ec0f556906748af7cc84b8060d284a
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-03-25 18:59:57 -0400

    Cat follows player

--- Assets/Cat/CatNavigation.cs
+++ Assets/Cat/CatNavigation.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatNavigation : MonoBehaviour
 {
     [SerializeField] Transform player;
-    // Start is called before the first frame update
-    void Start()
-    {
+    NavMeshAgent navMeshAgent;
+
+    void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        navMeshAgent.SetDestination(player.position);
     }
 }
