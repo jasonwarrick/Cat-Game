f280585d38dd62c448e04d7d33cd86604a44e3f3
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-04-10 20:19:46 -0400

    Basic (untested) meters implementation

--- Assets/Cat/CatBrain.cs
+++ Assets/Cat/CatBrain.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CatBrain : MonoBehaviour
@@ -11,22 +12,43 @@ public class CatBrain : MonoBehaviour
     [SerializeField] Transform nailpoint;
     [SerializeField] Transform medicinePoint;
 
-    float feedMeter = 0f;
-    float drinkMeter = 0f;
-    float litterMeter = 0f;
-    float playMeter = 0f;
-    float nailmeter = 0f;
-    float medicineMeter = 0f;
+    [SerializeField] float meterAmt;
+
+    Dictionary<string, float> meters = new Dictionary<string, float>();
 
     CatNavigation catNavigation;
 
     void Start() {
         catNavigation = GetComponent<CatNavigation>();
 
-        
+        meters.Add("feed", 0f);
+        meters.Add("drink", 0f);
+        meters.Add("litter", 0f);
+        meters.Add("play", 0f);
+        meters.Add("nail", 0f);
+        meters.Add("medicine", 0f);
     }
 
-    void Update() {
-        
+    void FixedUpdate() {
+        UpdateMeters();
+    }
+
+    void UpdateMeters() {
+        meters["feed"] += meterAmt;
+        meters["drink"] += meterAmt;
+        meters["litter"] += meterAmt;
+        meters["play"] += meterAmt;
+        meters["nail"] += meterAmt;
+        meters["medicine"] += meterAmt;
+    }
+
+    public void UpdateSpecMeter(string meter, float newMeterAmt) {
+        meters[meter] += newMeterAmt;
+    }
+
+    public float GetMeter(string meter) { 
+        if (meters.ContainsKey(meter)) {
+            return meters[meter]; 
+        } else { return -1; }
     }
 }
