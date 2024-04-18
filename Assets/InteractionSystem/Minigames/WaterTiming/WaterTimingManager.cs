eee9117f49709c99335ce54cbebebeacc7c16df5
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-04-18 17:13:46 -0400

    Completed basic water minigame

--- Assets/InteractionSystem/Minigames/WaterTiming/WaterTimingManager.cs
+++ Assets/InteractionSystem/Minigames/WaterTiming/WaterTimingManager.cs
@@ -1,20 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterTimingManager : MonoBehaviour
 {
-    bool waterIsRunning = false;
+    [SerializeField] float duration;
+    [SerializeField] float sweetSpotRange;
 
+    bool waterIsRunning = false;
+    bool inSweetSpot = false;
+
+    float timer = 0f;
+    float timerRatio = 0f;
+    float sweetSpotStart = 0f;
+
+    [SerializeField] MinigameManager minigameManager;
     [SerializeField] ParticleSystem waterParticles;
+    [SerializeField] Image indicator;
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+            SetSweetSpot();
             waterIsRunning = true;
+
             waterParticles.Play();
         } if (Input.GetMouseButtonUp(0)) {
-            waterIsRunning = false;
-            waterParticles.Stop();
+            if (inSweetSpot) {
+                MinigameWon();
+            } else if (waterIsRunning) { 
+                MinigameLost();
+            }
         }
+
+        if (waterIsRunning) {
+            UpdateIndicator();
+        }
+    }
+
+    void OnEnable() {
+        ResetMinigame();
+    }
+
+    void SetSweetSpot() {
+        sweetSpotStart = (duration / 2) + Random.Range(0f, duration / 2);
+
+        if (sweetSpotStart + sweetSpotRange > duration) {
+            sweetSpotStart -= sweetSpotStart + sweetSpotRange - duration;
+        }
+
+        Debug.Log("minimum is " + sweetSpotStart + "; maximum is " + (sweetSpotStart + sweetSpotRange));
+    }
+
+    void UpdateIndicator() {
+        timer += Time.deltaTime;
+
+        if (timer >= sweetSpotStart && timer <= sweetSpotStart + sweetSpotRange) {
+            indicator.color = Color.green;
+            inSweetSpot = true;
+        } else {
+            inSweetSpot = false;
+            indicator.color = Color.red;
+        }
+
+        if (timerRatio >= 1f) {
+            MinigameLost();
+        }
+    }
+
+    void MinigameLost() {
+        ResetMinigame();
+        waterIsRunning = false;
+        waterParticles.Stop();
+        AudioManager.instance.MinigameLost();
+    }
+
+    void MinigameWon() {
+        ResetMinigame();
+        inSweetSpot = false;
+        waterIsRunning = false;
+        waterParticles.Stop();
+        AudioManager.instance.MinigameWon();
+        minigameManager.CompleteMinigame();
+    }
+
+    void ResetMinigame() {
+        timer = 0f;
+        timerRatio = 0f;
+        indicator.color = Color.red;
     }
 }
