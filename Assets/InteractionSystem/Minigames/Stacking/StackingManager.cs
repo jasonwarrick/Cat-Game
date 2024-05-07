690430008ae1d3ac186276cb4c48cbc441dad895
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-05-06 20:29:23 -0400

    Launch is working

--- Assets/InteractionSystem/Minigames/Stacking/StackingManager.cs
+++ Assets/InteractionSystem/Minigames/Stacking/StackingManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class StackingManager : MonoBehaviour
 {
-    [SerializeField] float chargeRate;
+    [SerializeField] float powerFactor;
+    [SerializeField] float chargeTime;
     [SerializeField] Vector3 chargeDirection;
 
     int poopNeeded = 5;
     int poopMade = 0;
     bool charging = false;
+    float counter = 0f;
     
     [SerializeField] GameObject poop;
     [SerializeField] GameObject scoopObj;
+    [SerializeField] Transform spawnPoint;
     [SerializeField] MinigameManager minigameManager;
 
     void OnEnable() {
@@ -23,12 +26,36 @@ public class StackingManager : MonoBehaviour
         
     }
 
-    void Update() {
-        Scored();
+    void FixedUpdate() {
+        if (charging) {
+            counter += Time.deltaTime;
+            Debug.Log("charging = " + counter);
 
-        if (Input.GetMouseButton(0) && !charging) {
-            charging = true;
+            if (counter >= chargeTime) {
+                LaunchPoop();
+            }
         }
+    }
+
+    void Update() {
+        // Scored();
+
+        if (Input.GetMouseButtonDown(0) && !charging) {
+            charging = true;
+        } else if (Input.GetMouseButtonUp(0)) {
+            counter = 0f;
+            charging = false;
+        }
+    }
+
+    void LaunchPoop() {
+        Debug.Log("launch");
+        counter = 0f;
+        charging = false;
+        GameObject poopInstance = Instantiate(poop, spawnPoint.TransformPoint(Vector3.zero), Quaternion.identity);
+        // poopInstance.transform.position = spawnPoint.TransformPoint(Vector3.zero);
+        Debug.DrawRay(spawnPoint.position, spawnPoint.TransformDirection(chargeDirection), Color.green, 5f);
+        poopInstance.GetComponent<Rigidbody>().AddForce(spawnPoint.TransformDirection(chargeDirection) * powerFactor);
     }
 
     public void Missed() {
