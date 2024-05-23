846115e9ec8bf75c57f687ac00fb997cb7325c2f
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-05-23 17:25:59 -0400

    Audio Toggle

--- Assets/UI/Settings/SettingsManager.cs
+++ Assets/UI/Settings/SettingsManager.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
     public delegate void FilterToggled();
     public static FilterToggled filterToggled;
 
+    bool isMuted = false;
+
+    [SerializeField] AudioMixer master;
+
     public void ToggleFilterPressed() {
         filterToggled.Invoke();
     }
 
     public void ToggleAudioPressed() {
+        Debug.Log("toggle");
+        float newVol;
 
+        if (!isMuted) {
+            newVol =  -80f;
+        }  else {
+            newVol = 0f;
+        }
+
+        isMuted = !isMuted;
+        master.SetFloat("Volume", newVol);
     }
 }
